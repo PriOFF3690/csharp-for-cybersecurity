76877bffc5b9ce37dda0bfdcceff728fc2b1c82d
PriOFF3690 <princegokhale369@gmail.com>
2026-06-06 13:25:14 +0530

    Added UserInputApp

---
+++ UserInputApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String name;
+        Console.Write("Enter Your Name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Your name: "+name);
+
+        // To take input of the desired datatype use typecasting to convert the datatype.
+        Console.Write("\nEnter Your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your Age: "+age);
+    }
+}
